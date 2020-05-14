7bb36f47208ade15493e5d411a514f8df1f9c5eb
Paul Martin <paul.y.martin@johnlewis.co.uk>
2020-05-14 18:01:13 +0100

    Working solution with some tests missing

--- src/main/kotlin/Game.kt
+++ src/main/kotlin/Game.kt
@@ -1,4 +1,23 @@
+const val NUMBER_OF_BOAT_SQUARES = 18
 
+val BOARD = listOf(
+    Coordinate('A', 1)
+)
+
+fun createRow(letter: Char) = (0..9).toList().map { Coordinate(letter, it) }
+fun createBoard() = ('A'..'J').toList().flatMap { createRow(it) }
+
+tailrec fun doTheThing(previousResults: Results, board: List<Coordinate>): Results {
+    println(previousResults.size)
+    val nextGuess = findNextGuess(previousResults, board)
+    val newResults = makeAGuess(previousResults, nextGuess, createShoot(HttpClient()))
+
+    return if (newResults.isComplete()) {
+        newResults
+    } else {
+        doTheThing(newResults, board)
+    }
+}
 
 fun findNextGuess(previousResults: Results, board: List<Coordinate>): Coordinate {
     val remainingSquares = board - previousResults.map { it.coordinate }
@@ -14,4 +33,10 @@ fun makeAGuess(
 ): Results {
     val previousCoordinates = previousResults.map { it.coordinate }
     return shoot(previousCoordinates + nextGuess)
+}
+
+fun main() {
+    val board = createBoard()
+//    println(board)
+    println(doTheThing(emptyList(), board))
 }
--- src/main/kotlin/Results.kt
+++ src/main/kotlin/Results.kt
@@ -4,6 +4,4 @@ typealias Results = List<Result>
 
 data class Result(val coordinate: Coordinate, val status: Status)
 
-const val NUMBER_OF_BOAT_SQUARES = 18
-
 fun Results.isComplete(): Boolean = this.filter { it.status == SUNK }.size == NUMBER_OF_BOAT_SQUARES
