0843c1fa7af61ceaa25bec1fbf3ca90ef2e8a70b
Carolyn Biggar <carolyn.biggar@johnlewis.co.uk>
2020-05-14 14:24:08 +0100

    Map server results to statuses

--- src/main/kotlin/Shoot.kt
+++ src/main/kotlin/Shoot.kt
@@ -1,3 +1,6 @@
+import com.github.salomonbrys.kotson.get
+import com.google.gson.JsonParser
+
 typealias Shoot = (List<Coordinate>) -> List<Result>
 
 data class Coordinate(val x: String, val y: String)
@@ -8,8 +11,22 @@ enum class Status {
 
 fun createShoot(httpClient: HttpClient): Shoot {
     return fun(coordinates: List<Coordinate>): List<Result> {
+        val result = httpClient.get("")
         return coordinates.map { Result(it, Status.HIT) }
     }
+}
+
+fun mapResults(result: String): List<Status> {
+    val body = JsonParser().parse(result)
+    return body["results"].asJsonArray
+        .map {
+            when (it.asString) {
+                "H" -> Status.HIT
+                "M" -> Status.MISS
+                "S" -> Status.SUNK
+                else -> throw IllegalArgumentException("What's this? $it")
+            }
+        }
 }
 
 
--- src/test/kotlin/ShootKtTest.kt
+++ src/test/kotlin/ShootKtTest.kt
@@ -1,4 +1,4 @@
-import Status.HIT
+import Status.*
 import io.mockk.every
 import io.mockk.mockk
 import org.assertj.core.api.Assertions.assertThat
@@ -7,14 +7,23 @@ import org.junit.jupiter.api.Test
 internal class ShootKtTest {
 
     @Test
-    fun `something`() {
+    fun `should find a hit at B5`() {
         val httpClient = mockk<HttpClient>()
-        every { httpClient.get(any()) } returns """{ "results": ["H"] }"""
+        every { httpClient.get(any()) } returns """{ "results": ["H","M"] }"""
 
         val shoot = createShoot(httpClient)
 
-        val result = shoot(listOf(Coordinate("B", "5")))
+        val result = shoot(listOf(Coordinate("B", "5"), Coordinate("C", "6")))
 
-        assertThat(result).containsExactly(Result(Coordinate("B", "5"), HIT))
+        assertThat(result).containsExactly(
+            Result(Coordinate("B", "5"), HIT),
+            Result(Coordinate("C", "6"), MISS)
+        )
+    }
+
+    @Test
+    fun `should map results to status`() {
+        val result = mapResults("""{ "results": ["H","S","M"] }""")
+        assertThat(result).containsExactly(HIT, SUNK, MISS)
     }
 }
